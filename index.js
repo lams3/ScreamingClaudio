7b3687a55d75694a2ccbe1b34a85817a6a2ac434
Luca <lams3@cin.ufpe.br>
2018-03-10 18:39:45 -0300

    Refactoring
    Preparing for new features

--- index.js
+++ index.js
@@ -3,20 +3,56 @@ function resizeCanvas() {
   canvas.height = parseFloat(window.getComputedStyle(canvas).height);
 }
 
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < claudios.length; i++) {
+    if (sounds[claudios[i].n].playing(claudios[i].id)) {
+      ctx.drawImage(img, claudios[i].x - (img.width / 2), claudios[i].y - (img.height / 2));
+    }
+  }
+}
+
+function clear() {
+  for (let i = claudios.length - 1; i >= 0; i--) {
+    if (!sounds[claudios[i].n].playing(claudios[i].id)) {
+      claudios.splice(i, 1);
+    }
+  }
+}
+
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+
+let img = new Image();
+img.src = "assets/images/claudio.png";
+
+let soundNumber = 0;
+let sounds = [];
+for (let i = 1; i <= 10; i++) {
+  sounds.push(
+    new Howl({
+      src: [`assets/sounds/Claudio-${i}.mp3`]
+    })
+  );
+}
+
+let claudios = [];
+
 resizeCanvas();
 
-canvas.addEventListener('click', e => {
-  let img = new Image();
-  img.onload = function () {
-    ctx.drawImage(img, e.offsetX - (97 / 2), e.offsetY - (126 / 2));
-  }
-  img.src = "assets/images/claudio.png";
+window.addEventListener('resize', resizeCanvas);
 
-  let soundNumber = Math.floor(Math.random() * 10) + 1;
-  let sound = new Howl({
-    src: [`assets/sounds/Claudio-${soundNumber}.mp3`]
+canvas.addEventListener('click', e => {
+  let newNumber = soundNumber++ % 10;
+  claudios.push({
+    x: e.offsetX,
+    y: e.offsetY,
+    n: newNumber,
+    id: sounds[newNumber].play()
   });
-  sound.play();
 });
+
+setInterval(() => {
+  draw();
+  clear();
+}, 1000 / 60);
