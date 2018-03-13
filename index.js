53bf1e4fc2ee40863e95b8b07274edfb72cc3faa
Luca <lams3@cin.ufpe.br>
2018-03-12 21:55:02 -0300

    Movement Feature
    Cdded movement to the Claudios

--- index.js
+++ index.js
@@ -3,12 +3,62 @@ function resizeCanvas() {
   canvas.height = parseFloat(window.getComputedStyle(canvas).height);
 }
 
+function randomPoint() {
+  return {
+    x: Math.floor(Math.random() * canvas.width),
+    y: Math.floor(Math.random() * canvas.height)
+  }
+}
+
+function randomPoints(n) {
+  let list = [];
+  for (var i = 0; i < n; i++) {
+    list.push(randomPoint());
+  }
+  return list;
+}
+
+function fat(n) {
+  return (n <= 0) ? 1 : n * fat(n - 1);
+}
+
+function b(i, n, t, p) {
+  let point = {};
+  let c = (fat(n) / (fat(i) * fat(n - i)));
+  point.x = c * Math.pow(1 - t, n - i) * Math.pow(t, i) * p.x;
+  point.y = c * Math.pow(1 - t, n - i) * Math.pow(t, i) * p.y;
+  return point;
+}
+
+function getPoint(path, t) {
+  let p = {
+    x: 0,
+    y: 0
+  }
+  for (var i = 0; i < path.length; i++) {
+    p.x += b(i, path.length - 1, t, path[i]).x;
+    p.y += b(i, path.length - 1, t, path[i]).y;
+  }
+  return p;
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < claudios.length; i++) {
     if (sounds[claudios[i].n].playing(claudios[i].id)) {
       ctx.drawImage(img, claudios[i].x - (img.width / 2), claudios[i].y - (img.height / 2));
     }
+  }
+}
+
+function update() {
+  for (var i = 0; i < claudios.length; i++) {
+    let t1 = sounds[claudios[i].n].seek(claudios[i].id);
+    let t2 = sounds[claudios[i].n].duration();
+    let t = t1 / t2;
+    let point = getPoint(claudios[i].path, t);
+    claudios[i].x = point.x;
+    claudios[i].y = point.y;
   }
 }
 
@@ -48,11 +98,19 @@ canvas.addEventListener('click', e => {
     x: e.offsetX,
     y: e.offsetY,
     n: newNumber,
-    id: sounds[newNumber].play()
+    id: sounds[newNumber].play(),
+    path: [
+      {
+        x: e.offsetX,
+        y: e.offsetY
+      },
+      ...randomPoints(3)
+    ]
   });
 });
 
 setInterval(() => {
   draw();
+  update();
   clear();
 }, 1000 / 60);
